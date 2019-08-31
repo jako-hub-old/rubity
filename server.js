26c4dab4769926231e1a0500e81b1018803dec98
Jorge Alejandro Quiroz Serna <jakop.box@gmail.com>
2019-08-31 13:28:56 -0500

    try with server

--- server.js
+++ server.js
@@ -7,10 +7,9 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 app.listen(port);
