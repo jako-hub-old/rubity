4d5948cc4796fb56cf288e13578d47760120164d
Jorge Alejandro Quiroz Serna <jakop.box@gmail.com>
2019-08-31 13:35:50 -0500

    deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
+
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
